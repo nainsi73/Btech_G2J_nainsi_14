bd8527ae754df2295805ebdf2339694cd56ba9c7
nainsi73 <25116cn051@glbitm.ac.in>
2026-02-11 12:03:08 +0530

    Add files via upload

---
+++ nainsi02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3,greatest;
+    printf("enter three number: ");
+    scanf("%d %d %d", &num1,&num2,&num3);
+    greatest=(num1 >= num2 && num1 >= num3) ? num1 : (num2 >= num1 && num2 >= num3) ? num2 : num3;
+    
+    printf("%d is the grestest number.\n", greatest);
+    return 0;
+}
